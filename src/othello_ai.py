27519316d4921d62be266f7901d692aaf52c331d
Anne Havunen <anne.havunen@helsinki.fi>
2023-02-10 11:48:15 +0200

    Järjestä siirrot ennen minimaxia

--- src/othello_ai.py
+++ src/othello_ai.py
@@ -16,10 +16,10 @@ class OthelloAI:
     def valitse_siirto(self, syvyys, othello):
         alfa = -100000
         beta = 100000
-        mahdolliset_siirrot = othello.mahdolliset_siirrot("valkoinen")
+        jarjestetyt_siirrot = self.jarjesta_siirrot(othello)
         paras_siirto = None
         paras_arvo = alfa
-        for siirto in mahdolliset_siirrot:
+        for siirto in jarjestetyt_siirrot:
             kopio_othello = self.tee_siirto_ja_kopioi_othello(othello, siirto, "valkoinen")
             arvo = self.minimax(syvyys - 1, kopio_othello, alfa, beta, False)
             if arvo > paras_arvo:
@@ -38,10 +38,6 @@ class OthelloAI:
             return 0
         if syvyys == 0:
             return self.arvioi_pelilauta(othello)
-        if maksimoi_pelaaja and not othello.mahdolliset_siirrot("valkoinen"):
-            self.minimax(syvyys - 1, othello, alfa, beta, False)
-        if not maksimoi_pelaaja and not othello.mahdolliset_siirrot("musta"):
-            self.minimax(syvyys - 1, othello, alfa, beta, True)
 
         if maksimoi_pelaaja:
             paras_arvo = alfa
@@ -72,6 +68,22 @@ class OthelloAI:
                 elif pelilauta[i][j] == "x":
                     arvio -= PAINOT[i][j]
         return arvio
+
+    def jarjesta_siirrot(self, othello):
+        mahdolliset_siirrot = othello.mahdolliset_siirrot("valkoinen")
+        muutos_siirto = []
+        for siirto in mahdolliset_siirrot:
+            lauta = othello.hae_pelilauta()
+            kopiolauta = deepcopy(lauta)
+            kopio_othello = Othello(kopiolauta)
+            kopio_othello.tee_siirto(siirto, "valkoinen")
+            muutos = kopio_othello.hae_mustat_ja_valkoiset()[1] - othello.hae_mustat_ja_valkoiset()[1]
+            muutos_siirto.append((muutos, siirto))
+        muutos_siirto.sort()
+        jarjestetyt_siirrot = []
+        for luku_siirto in muutos_siirto:
+            jarjestetyt_siirrot.append(luku_siirto[1])
+        return jarjestetyt_siirrot
 
     def tee_siirto_ja_kopioi_othello(self, othello, siirto, vari):
         othello.tee_siirto(siirto, vari)
